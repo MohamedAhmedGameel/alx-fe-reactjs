d6147e53708c9a8ec991ba7ed2423b8487cc0dd2
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-11-10 20:44:09 +0200

    alx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./contexts/UserContext";
+import UserContext from "./UserContext";
 
 function App() {
   // Example user data
