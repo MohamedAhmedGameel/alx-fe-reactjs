8182c0b191a184cf8bc68b8609e8dfd3ad48d9b8
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-11-10 21:02:25 +0200

    alx

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div style={{ padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
