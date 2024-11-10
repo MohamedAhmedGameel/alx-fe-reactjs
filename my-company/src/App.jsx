235b4eea08833f48c805bdf63d82e41316b46276
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-11-10 21:08:26 +0200

    alx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import About from "./About";
+import About from "../../my-company/src/About";
 import Services from "./Services";
 import Contact from "./Contact";
 import Navbar from "./Navbar";
