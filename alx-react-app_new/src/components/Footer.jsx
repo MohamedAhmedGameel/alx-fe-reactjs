5c45dbc13ea92aa02999ec7eb93be920a5ac557b
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-11-10 20:32:23 +0200

    alx

---
+++ alx-react-app_new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ border: "2px solid green", padding: "10px", margin: "10px" }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app_new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main style={{ border: "2px solid blue", padding: "4px", margin: "6px" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
