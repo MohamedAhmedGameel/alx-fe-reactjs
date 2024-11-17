c5e66f39e38b1c3c25295e019155bca5797429d7
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-11-17 20:35:47 +0200

    alx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,16 @@
-// src/components/DeleteRecipeButton.js
+// src/components/DeleteRecipeButton.jsx
 
+import { useNavigate } from "react-router-dom";
 import { useRecipeStore } from "../store/recipeStore";
 
 const DeleteRecipeButton = ({ recipeId }) => {
+  const navigate = useNavigate();
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this recipe?")) {
-      deleteRecipe(recipeId);
+      deleteRecipe(recipeId); // Delete the recipe from the store
+      navigate("/"); // Navigate back to the home page or a different route after deletion
     }
   };
 
