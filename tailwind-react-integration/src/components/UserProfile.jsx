6efe7fdc575367747f296c006f991d025a0282a7
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2024-12-01 23:18:12 +0200

    alx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
@@ -12,7 +12,8 @@ function UserProfile() {
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center hover:text-blue-500 hover:shadow-xl">
-        Developer at Example Co. Loves to write code and explore new technologies.
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
       </p>
     </div>
   );
